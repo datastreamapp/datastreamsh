4bfdc45ed587519c152ce4ffc70a8ab172520ee4
will Farrell <willfarrell@proton.me>
2023-03-02 21:20:04 -0700

    feat: init commit

--- README.md
+++ README.md
@@ -1,2 +1,63 @@
-# datastreamsh
-DataSteam API helper in Shell
+<h1 align="center">
+  <img src="https://raw.githubusercontent.com/datastreamapp/api-docs/main/docs/images/datastream.svg?sanitize=true" alt="DataStream Logo" width="400">
+  <br/>
+  DataStream.sh
+  <br/>
+  <br/>
+</h1>
+<p align="center">
+  DataStream.org API helper. See <a href="https://github.com/datastreamapp/api-docs/tree/main/docs">API documentation</a> for query string values and structure.
+</p>
+
+## Install (MacOS)
+
+```bash
+curl -o /usr/local/bin/datastreamsh https://raw.githubusercontent.com/datastreamapp/datastreamsh/main/datastream.sh
+chmod +x /usr/local/bin/datastreamsh
+```
+
+## Use
+
+This package has been tested on MacOS.
+
+```
+datastreamsh command [options...]
+```
+
+### Commands
+
+- `setup`: Prompts for API key, will be save to `~/.datastream` in plain text. Not setting up will prompt for `x-api-key` on ever command.
+- `metadata`: Wraps /v1/odata/v4/Metadata API endpoint
+- `locations`: Wraps /v1/odata/v4/Locations API endpoint
+- `observations`: Wraps /v1/odata/v4/Observations API endpoint
+- `records`: Wraps /v1/odata/v4/Records API endpoint
+
+### Options
+
+- `--select`: What parameters to be returned
+- `--filter`: What you want to , in OData format
+- `--format`: Print output in alternative format (Allowed: `JSONSTREAM`, `CSV`; Default: `JSONSTREAM`)
+- `--top`: Number of results to return per request (Default: 10000)
+- `--domain`: Use to point at testing environments
+
+### Example: Setup
+
+```bash
+# datastreamsh setup
+$ datastreamsh setup
+x-api-key: # copy and paste API key here and press enter
+```
+
+### Example: Basic
+
+```bash
+# datastreamsh command [options...]
+$ datastreamsh locations --select "..." --filter "..."
+```
+
+### Example: save as CSV to file
+
+```bash
+# datastreamsh command --format CSV --select <select> [options...]
+$ datastreamsh records --format CSV --select "..." --filter "..." > file.csv
+```
---
+++ datastream.sh
@@ -0,0 +1,198 @@
+#!/usr/bin/env sh
+#set -x
+
+# ./datastream.sh --select "Id,DOI" --filter "" --api-key ""
+# columns="Id,DOI"
+# echo ${columns} > output.csv
+# ./datastream.sh --select "${columns}" --filter "" --api-key "" | while read line; do; echo $line | jq -r '. | [.Id, .DOI] | @csv' >> output.csv ; done
+# OS Requirements
+if ! type "curl" > /dev/null; then
+  echo "curl is required, see https://curl.se/download.html"
+  REQUIRED=FAIL
+fi
+
+if ! type "jq" > /dev/null; then
+  echo "jq is required, see https://stedolan.github.io/jq/download/"
+  REQUIRED=FAIL
+fi
+
+if [ -n "${REQUIRED}" ]; then
+  exit 1
+fi
+
+# TODO add in version control
+
+# Defaults
+FORMAT=JSONSTREAM
+TOP=10000
+DOMAIN=api.datastream.org
+APIKEY_PATH=~/.datastream
+
+# Arguments
+POSITIONAL=()
+while [[ $# -gt 0 ]]; do
+	case ${1} in
+		--select)
+		SELECT="${2}"
+		shift # past argument
+		shift # past value
+		;;
+		--filter)
+		FILTER="${2}"
+		shift # past argument
+		shift # past value
+		;;
+		--top)
+		TOP="${2}"
+		shift # past argument
+		shift # past value
+		;;
+		--domain)
+		DOMAIN="${2}"
+		shift # past argument
+		shift # past value
+		;;
+		--format)
+		FORMAT="${2}"
+		shift # past argument
+		shift # past value
+		;;
+		--help)
+		echo "https://github.com/datastreamapp/datastreamsh"
+		shift # past argument
+		;;
+		*)    # unknown option
+		POSITIONAL+=("$1") # save it in an array for later
+		shift # past argument
+		;;
+	esac
+done
+set -- "${POSITIONAL[@]}" # restore positional parameters
+
+# Arg requirements
+
+
+if [ "${FORMAT}" == "CSV" ] && [ -z "${SELECT}" ]; then
+  echo "--select is required when using --csv"
+  exit 1
+elif [ "${FORMAT}" == "CSV" ] && [ -n "${SELECT}" ]; then
+  CSV_SELECT=".${SELECT//,/, .}"
+fi
+
+APIKEY=$(cat ~/.datastream 2> /dev/null)
+if [ -z "${APIKEY}" ]; then
+  read -sp "x-api-key: " APIKEY
+fi
+
+if [ "${1}" === "setup" ]; then
+  echo "${APIKEY}" > ~/.datastream
+  exit 0
+fi
+#
+
+# Functions
+function request {
+	if [ -z "${1}" ]; then
+	  echo "Error: No URL"
+	  exit 1
+	fi
+	res=$(curl -sG ${1} -H "Accept: application/vnd.api+json" -H "x-api-key: ${APIKEY}")
+	
+	err=$(echo ${res} | jq -c '.errors')
+	if [ "${err}" != "null" ]; then
+	  echo "${err}"
+	  exit 1
+	fi
+	if [ "${FORMAT}" == "CSV" ]; then
+	  echo ${res} | jq -rc ".value[] | [${CSV_SELECT}] | @csv"
+	else
+	  echo ${res} | jq -c '.value[]'
+	fi
+	
+	# recursive
+	url=$(echo ${res} | jq -r '."@odata.nextLink"')
+	if [ "${url}" != "null" ]; then
+	  request ${url}
+	fi
+}
+
+function urlconcat {
+	queryTop="\$top=${TOP}"
+	querySelect=
+	if [ "${2}" != "" ]; then
+		querySelect="&\$select="$(urlencode "${2}")
+	fi
+	queryFilter=
+	if [ "${3}" != "" ]; then
+		queryFilter="&\$filter="$(urlencode "${3}")
+	fi
+	echo "https://${DOMAIN}${1}?${queryTop}${querySelect}${queryFilter}"
+}
+
+function urlencode {
+	# source: https://github.com/SixArm/urlencode.sh/blob/main/urlencode.sh
+
+	old_lang=$LANG
+	LANG=C
+	
+	old_lc_collate=$LC_COLLATE
+	LC_COLLATE=C
+
+	local length="${#1}"
+	for (( i = 0; i < length; i++ )); do
+		local c="${1:i:1}"
+		case $c in
+			[a-zA-Z0-9.~_-]) printf "$c" ;;
+			*) printf '%%%02X' "'$c" ;;
+		esac
+	done
+
+	LANG=$old_lang
+	LC_COLLATE=$old_lc_collate
+}
+matchPartitionedRegExp='(^Id| Id|^LocationId| LocationId)'
+function partitionRequest {
+	if [[ ${3} =~ ${matchPartitionedRegExp} ]]; then
+	  url=$(urlconcat "${1}" "${2}" "${3}")
+	  request ${url}
+	  return 
+	fi
+	OUTPUT_CSV="${CSV}"
+	CSV=FALSE
+	locations "Id" "${3/LocationId/Id}" | while read line; do
+	  CSV="${OUTPUT_CSV}"
+	  locationId=$(echo $line | jq '.Id')
+	  filter="LocationId eq ${locationId}"
+	  if [ "${3}" != "" ]; then
+		  filter+=" and ${3}"
+	  fi
+	  url=$(urlconcat "${1}" "${2}" "${filter}")
+	  request ${url}
+	done
+}
+
+# Commands
+function metadata {
+	# Pagination not supported on endpoint
+	TOP=10000
+	url=$(urlconcat /v1/odata/v4/Metadata "${1}" "${2}")
+	request ${url}
+}
+
+function locations {
+	url=$(urlconcat /v1/odata/v4/Locations "${1}" "${2}")
+	request ${url}
+}
+
+function observations {
+	partitionRequest /v1/odata/v4/Observations "${1}" "${2}"
+}
+
+function records {
+	partitionRequest /v1/odata/v4/Records "${1}" "${2}"
+}
+
+if [ "${FORMAT}" == "CSV" ]; then
+  echo "${SELECT}"
+fi
+${1} "${SELECT}" "${FILTER}"
